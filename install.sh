072362449c813a3c5e8327c93d1feb459aee6383
Chris Gruel <chris.gruel@armory.io>
2021-06-10 13:13:46 -0400

    better output

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ brew install yq
 echo "::debug::\$cmd: $1"
 RESULT=$(eval "$1")
 echo "::debug::\$RESULT: $RESULT"
-echo "$RESULT" >| "$GITHUB_ACTION_PATH/.result"
+echo "::set-env name=jqresult::$RESULT"
