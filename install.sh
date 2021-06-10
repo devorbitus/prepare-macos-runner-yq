2485a836d8285cf683ea81b56713bcce877950e6
Chris Gruel <chris.gruel@armory.io>
2021-06-10 14:14:58 -0400

    try getting the result

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ brew update
 brew install yq
 
 echo "::debug::\$cmd: $1"
-RESULT=$(eval "$1")
-echo "::debug::\$RESULT: $RESULT"
-echo "::set-output name=result::$RESULT"
+echo "::set-output name=result::$(eval "$1")"
