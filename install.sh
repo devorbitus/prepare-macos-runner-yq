2a4bc3d07da1f31b25fe7ac546dd132068f32362
Chris Gruel <chris.gruel@armory.io>
2021-06-10 14:42:20 -0400

    extra debugging

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ echo "::debug::\$cmd: $1"
 brew update
 brew install yq
 
+# print this again because installing uses a lot of lines
+echo "::debug::\$cmd: $1"
 echo "::set-output name=result::$(eval "$1")"
