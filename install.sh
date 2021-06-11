1fd935be55b494037b3465d9a8dab9a04f1ab135
Chris Gruel <chris.gruel@armory.io>
2021-06-11 09:45:06 -0400

    lil error handling

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 
 echo "::debug::\$cmd: $1"
 
-brew update
-brew install yq
+if ! command -v yq &> /dev/null
+then
+    echo "yq command could not be found, so installing it"
+    if ! command -v brew &> /dev/null
+    then
+        echo "::error::brew command could not be found, so aborting"
+        exit 1
+    else
+        brew update
+    fi
+    brew install yq
+fi
 
 # print this again because installing uses a lot of lines
 echo "::debug::\$cmd: $1"
